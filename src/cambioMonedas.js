fa5962e11f00e3b7a2c18a5e9f48d240de3b61ab
Bruno Marquez <bruno.marquez@ucb.edu.bo>
2022-05-31 12:05:48 -0400

    devuelve 5 y 2 si se ingresa 7

--- src/cambioMonedas.js
+++ src/cambioMonedas.js
@@ -1,6 +1,11 @@
 
 function cambioMonedas(monedas){
-    return monedas;
+    let res = monedas;
+    if (monedas - 5 >= 0){
+        res = [5, monedas - 5];
+        return res;
+    }
+    return res;
 }
 
 export default cambioMonedas;
--- src/cambioMonedas.test.js
+++ src/cambioMonedas.test.js
@@ -5,7 +5,14 @@ describe("Cambio de Monedas", () => {
     it("Para 5 deberia devolver 5", () => {
       let cambio = cambioMonedas(5);
   
-      expect(cambio).toEqual(5);
+      expect(cambio[0]).toEqual(5);
     });
+
+    it("Para 7 deberia devolver 5 y 2", () => {
+        let cambio = cambioMonedas(7);
+    
+        expect(cambio[0]).toEqual(5);
+        expect(cambio[1]).toEqual(2);
+      });
 
   }); 
